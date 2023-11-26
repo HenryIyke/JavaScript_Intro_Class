9e66b53d97959800fae9eb5c0ef4dfaf5598f501
HenryIyke <realhenryiyke@gmail.com>
2023-11-26 22:11:34 +0100

    Final commit on Operators

---
+++ Operators_Practice_Questions/Question10.js
@@ -0,0 +1,7 @@
+const rad = parseInt(prompt("Enter radius"));
+const pi = 3.14;
+
+const a = (pi * rad * rad);
+const circ = (2 * pi * rad);
+
+alert("The area of the circle is " + a + " and The circumference is " + circ);
---
+++ Operators_Practice_Questions/Question11.js
@@ -0,0 +1,5 @@
+const hours = parseInt(prompt("Enter the number of hours worked"));
+const rate = parseInt(prompt("Enter your rate per hour"));
+
+const pay = hours * rate;
+alert("Your weekly earning is " + pay);
